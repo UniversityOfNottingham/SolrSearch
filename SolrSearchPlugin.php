381b247026f9235034111b1b5be7a2ef4719e88a
Adam Cooper <adam.cooper@nottingham.ac.uk>
2016-06-02 11:41:57 +0100

    Fix bizarre typo bug that was stopping upgrades from happening.

--- SolrSearchPlugin.php
+++ SolrSearchPlugin.php
@@ -356,7 +356,7 @@ SQL
     {
         $this->_db
             ->getTable('SolrSearchField')
-            ->installGenericFacet($slub, $label);
+            ->installGenericFacet($slug, $label);
     }
 
 
